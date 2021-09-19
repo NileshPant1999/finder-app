4be99ce92fb5e12c362a208d06afdf0425efe313
Nilesh Pant <nileshpant6969@gmail.com>
2021-09-20 01:53:59 +0530

    Rotation of card

--- app/App.js
+++ app/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import 'react-native-gesture-handler';
 import Card from './src/components/FinderCard';
 import users from './assets/data/users';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withSpring,
+  withTiming,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  interpolate,
 } from 'react-native-reanimated';
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
 
 const jeff = {
   name: 'Jeff Bezos',
@@ -15,25 +30,50 @@ const jeff = {
   bio: 'A dude with gal with fuel CEO, entrepreneur born in 1964, Jeffrey Bezos',
 };
 
+const ROTATION = 60;
+
 const App = () => {
-  const sharedValue = useSharedValue(1);
+  const {width: screenWidth} = useWindowDimensions();
+
+  const hiddenTranslateX = 2 * screenWidth;
+
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
+      'deg',
+  );
 
   const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
-        translateX: sharedValue.value * 100,
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
       },
     ],
   }));
 
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: () => {
+      console.log('sifei');
+    },
+  });
+
   return (
     <View style={styles.pageContainer}>
-      <Animated.View style={[styles.animatedCard, cardStyle]}>
-        <Card user={users[2]} />
-      </Animated.View>
-      <Pressable onPress={() => (sharedValue.value = Math.random())}>
-        <Text>Change value</Text>
-      </Pressable>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+          <Card user={users[2]} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
@@ -48,6 +88,7 @@ const styles = StyleSheet.create({
   },
   animatedCard: {
     width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
