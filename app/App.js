7c05248344434432c61c55afa334c996bac0af0d
Nilesh Pant <nileshpant6969@gmail.com>
2021-09-21 00:59:05 +0530

    handle swipes

--- app/App.js
+++ app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {
   Pressable,
   StyleSheet,
@@ -17,22 +17,23 @@ import Animated, {
   useAnimatedGestureHandler,
   useDerivedValue,
   interpolate,
+  runOnJS,
 } from 'react-native-reanimated';
 import {
   PanGestureHandler,
   TapGestureHandler,
 } from 'react-native-gesture-handler';
 
-const jeff = {
-  name: 'Jeff Bezos',
-  image:
-    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
-  bio: 'A dude with gal with fuel CEO, entrepreneur born in 1964, Jeffrey Bezos',
-};
-
 const ROTATION = 60;
+const SWIPE_VELOCITY = 500;
 
 const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
+
   const {width: screenWidth} = useWindowDimensions();
 
   const hiddenTranslateX = 2 * screenWidth;
@@ -55,6 +56,23 @@ const App = () => {
     ],
   }));
 
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.9, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.8, 1],
+    ),
+  }));
+
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, context) => {
       context.startX = translateX.value;
@@ -62,16 +80,33 @@ const App = () => {
     onActive: (event, context) => {
       translateX.value = context.startX + event.translationX;
     },
-    onEnd: () => {
-      console.log('sifei');
+    onEnd: event => {
+      if (Math.abs(event.velocityX)) {
+        console.log('ubiub', event);
+        translateX.value = withSpring(0);
+        return;
+      }
+      translateX.value = withSpring(hiddenTranslateX);
+
+      runOnJS(setCurrentIndex)(currentIndex + 1);
     },
   });
 
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
+
   return (
     <View style={styles.pageContainer}>
+      <View style={styles.nextCardContainer}>
+        <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+          <Card user={nextProfile} />
+        </Animated.View>
+      </View>
       <PanGestureHandler onGestureEvent={gestureHandler}>
         <Animated.View style={[styles.animatedCard, cardStyle]}>
-          <Card user={users[2]} />
+          <Card user={currentProfile} />
         </Animated.View>
       </PanGestureHandler>
     </View>
@@ -88,7 +123,12 @@ const styles = StyleSheet.create({
   },
   animatedCard: {
     width: '100%',
-    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
   },
